e97a16864dce07a3128528db9911b7e9107f913e
Thanh Luong C <thanh.luong@supremetech.vn>
2020-12-29 08:33:51 +0700

    create is check and edit todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Switch from "react-bootstrap/esm/Switch";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.scss";
 import { AddTodoList } from "./components/addTodoList/addTodoList";
+import { EditTodoList } from "./components/editTodoList/editTodoList";
 import { TodoList } from "./components/todoList/todoList";
 
 function App() {
   return (
     <div>
-      <div className="layout">
-        <h2>Todos</h2>
-        <div className="content-todo">
-          <AddTodoList />
-          <TodoList />
+      <Router>
+        <div className="layout">
+          <h2>Todos</h2>
+          <div className="content-todo">
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <Fragment>
+                    <AddTodoList />
+                    <TodoList />
+                  </Fragment>
+                )}
+              />
+              <Route path="/edit/:id" component={EditTodoList} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     </div>
   );
 }
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,16 @@
 import ACTION_TYPES from "../constants/actionType";
 
 export const createTodo = (value: object) => ({
-    type: ACTION_TYPES.ADD_TODO,
-    payload: value
-  });
+  type: ACTION_TYPES.ADD_TODO,
+  payload: value,
+});
 
-  export const deleteTodo = (id: number) => ({
-    type: ACTION_TYPES.DELETE_TODO,
-    payload: id
-  });
+export const deleteTodo = (value: any) => ({
+  type: ACTION_TYPES.DELETE_TODO,
+  payload: value,
+});
+
+export const updateTodo = (value: object) => ({
+  type: ACTION_TYPES.UPDATE_TODO,
+  payload: value,
+});
